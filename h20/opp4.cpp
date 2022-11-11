2e8d5b8ff915494276be5496ddb450a3977003c4
KjetilMainPc <kjetilindrehus@gmail.com>
2022-11-11 22:22:03 +0100

    Eksamen Høst 2020: Fiket på index

--- h20/opp4.cpp
+++ h20/opp4.cpp
@@ -5,8 +5,19 @@ using namespace std;
 
 
 
-const int s1 = 13;
-int arr1[s1] = {0,1,1,2,2,3,4,5,6,7,7,7,8};
+const int s1 = 12;
+int arr1[s1+1] = {0,1,1,2,2,3,4,5,6,7,7,7,8};
+
+
+
+void display(int a[], const int n){
+    cout << "N = "<<n<< ": [ ";
+
+    for(int i = 1; i<n+1;i++){
+        cout << a[i] <<" ";
+    }
+    cout << "]" << endl;
+}
 
 
 /**
@@ -24,49 +35,39 @@ int fjernDuplikater(int a[], const int n){
     if(n == 0 || n == 1) return n; 
 
 
-
     int dobble = 0; 
 
-    for(int i = 1; i<n-1-dobble;i++){
+    for(int i = 1; i<n-dobble+1;i++){
+
         int curr = a[i]; 
         int neste = a[i+1];
 
-        //cout << "CURRENT= " << curr << " , NEXT = " << neste << endl; 
-
+    
         if(curr == neste){
-             
-            //FLytt alle en fram
             
+            //FLytt alle en fram
             int j = i; 
             for(j = i; j<n-dobble;j++){
                 a[j] = a[j+1];
             }
-            a[j+1] = 0; 
+            a[j] = 0; 
             
             //Dersom element nr i ikkje var 0, så kan det være fleire doble, så vi går tilbake en plass
             if(a[i] != 0) i = i - 1; 
 
-
             //Øker antall duplikater 
-            dobble++;
+            dobble++; 
+
             
         }
     }
 
     //Returnerer det orginale antall elementer (n - 1), minus antall duplikater (SKAL EGENTLIG VÆRE N - DOBBLE, MEN PGA SLIK DET ER SATT OPP)
-    return n - dobble - 1; 
+    return n - dobble; 
 }
 
 
 
-void display(int a[], const int n){
-    cout << "N = "<<n<< ": [ ";
-
-    for(int i = 1; i<n;i++){
-        cout << a[i] <<" ";
-    }
-    cout << "]" << endl;
-}
 
 
 
